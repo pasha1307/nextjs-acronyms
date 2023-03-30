85a77562853d11eb60f54d621809ba48fddc5684
Pavel Krotov <pavelkrotov@ppk-mbp-m1.local>
2023-03-30 11:41:32 -0400

    auto-complete is added

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
+"use client"
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
+import AcroSearch from "@/app/components/acro-search";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,10 +10,11 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
+        {/*<p>*/}
+        {/*  Get started by editing&nbsp;*/}
+        {/*  <code className={styles.code}>src/app/page.js</code>*/}
+        {/*</p>*/}
+        <AcroSearch />
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
