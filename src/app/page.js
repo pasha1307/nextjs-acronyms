88a3b5822172093179d527a0e157a88ca6c4cb84
Pavel Krotov <pavelkrotov@ppk-mbp-m1.local>
2023-03-30 14:21:51 -0400

    extra logo is removed

--- src/app/page.js
+++ src/app/page.js
@@ -27,23 +27,23 @@ export default function Home() {
         {/*  <code className={styles.code}>src/app/page.js</code>*/}
         {/*</p>*/}
 
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+        {/*<div>*/}
+          {/*<a*/}
+          {/*  href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"*/}
+          {/*  target="_blank"*/}
+          {/*  rel="noopener noreferrer"*/}
+          {/*>*/}
+          {/*  By{' '}*/}
+          {/*  <Image*/}
+          {/*    src="/vercel.svg"*/}
+          {/*    alt="Vercel Logo"*/}
+          {/*    className={styles.vercelLogo}*/}
+          {/*    width={100}*/}
+          {/*    height={24}*/}
+          {/*    priority*/}
+          {/*  />*/}
+          {/*</a>*/}
+        {/*</div>*/}
       </div>
 
       <div className={styles.center}>
