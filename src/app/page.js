d65183a5b702bde8e9bb86033172cc0fc14b3c12
Pavel Krotov <pavelkrotov@ppk-mbp-m1.local>
2023-03-30 12:34:08 -0400

    basic layout style is added

--- src/app/page.js
+++ src/app/page.js
@@ -12,17 +12,18 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <div>
+        <div className={styles.searchblock}>
           <h2>{title.title}</h2>
           <p>{title.subtitle}</p>
           <p>{title.about}</p>
+          <AcroSearch />
         </div>
 
         {/*<p>*/}
         {/*  Get started by editing&nbsp;*/}
         {/*  <code className={styles.code}>src/app/page.js</code>*/}
         {/*</p>*/}
-        <AcroSearch />
+
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
